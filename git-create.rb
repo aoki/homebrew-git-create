f2da49cdc6b90a87663f38aee1cbc1da3d316f12
Yoshiki Aoki <ringohub@gmail.com>
2018-03-14 13:06:16 +0900

    Bump up to 0.2.0

--- git-create.rb
+++ git-create.rb
@@ -1,9 +1,9 @@
 class GitCreate < Formula
   desc "Create Repository and clone via ghq"
   homepage "https://github.com/ringohub/git-create"
-  url "https://github.com/ringohub/git-create/releases/download/v0.1.0/git-create"
-  version "0.1.0"
-  sha256 "3ec6970107b601592c5315f97d841fc4acaba86a939a60eda64819b6cf6d184b"
+  url "https://github.com/ringohub/git-create/releases/download/v0.2.0/git-create"
+  version "0.2.0"
+  sha256 "ba6258b8ae8efac75fb3f8df489b957effdebe0ef72fc3e7bf0ca9d423a1021d"
 
   depends_on "jq"
   depends_on "ghq"
